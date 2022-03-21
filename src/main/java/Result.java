9feb52cecc3f2e7a70b99bb8dfdc5b44deb52def
Dave4President <92023604+Dave4President@users.noreply.github.com>
2022-03-21 20:35:51 +0100

    Ich versuche die endgueltige Loesung nachzureichen

---
+++ src/main/java/Result.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Result {
+
+    static int students;
+
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+
+
+
+
+
+
+
+
+        return null;
+
+    }
+
+    public void setStudents(int newStudents) {
+        this.students = newStudents;
+    }
+
+    public static int getStudents() {
+        return students;
+    }
+
+
+
+
+}
